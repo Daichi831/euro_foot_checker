80bb383bfa4bbb4ca7c5588d82ccaf348094af5c
kanekodaichi <knkdic.bfc@gmail.com>
2021-10-20 14:37:52 +0900

    teamテーブルにleague_idの外部キー制約を設定

--- db/migrate/20211019133107_create_teams.rb
+++ db/migrate/20211019133107_create_teams.rb
@@ -3,6 +3,7 @@ class CreateTeams < ActiveRecord::Migration[6.1]
     create_table :teams do |t|
       t.string :name
       t.integer :standing
+      t.references :league, foreign_key: true
 
       t.timestamps
     end
