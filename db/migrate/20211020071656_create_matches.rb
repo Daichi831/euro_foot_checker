fe42c460bf8483471c31e65cd32155579c2cf8f5
kanekodaichi <knkdic.bfc@gmail.com>
2021-10-22 19:40:46 +0900

    matchモデルに外部キーを設定した

--- db/migrate/20211020071656_create_matches.rb
+++ db/migrate/20211020071656_create_matches.rb
@@ -6,7 +6,12 @@ class CreateMatches < ActiveRecord::Migration[6.1]
       t.integer :home_team_score
       t.integer :away_team_score
 
+      t.integer :home_team_id, null: false
+      t.integer :away_team_id, null: false
+
       t.timestamps
     end
+    add_foreign_key :matches, :teams, column: :home_team_id
+    add_foreign_key :matches, :teams, column: :away_team_id
   end
 end
