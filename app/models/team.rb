10d71e09ede0f0a1fdee7137ff0977b693213af8
kanekodaichi <knkdic.bfc@gmail.com>
2021-10-26 22:28:49 +0900

    TeamとMatchの関連付けを修正

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Team < ApplicationRecord
-  has_many :matches
+  has_many :home_matches, class_name: 'Match', foreign_key: 'home_team_id'
+  has_many :away_matches, class_name: 'Match', foreign_key: 'away_team_id'
   belongs_to :league
 end
