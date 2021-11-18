d216d7a9e3f063473e39ac9b9babf8ce637baec5
kanekodaichi <knkdic.bfc@gmail.com>
2021-11-18 18:15:22 +0900

    deviseのコントローラーファイルを生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
   root to: 'matches#index'
   resources :matches
   namespace :api do
