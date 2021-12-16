90ab7ca97d59b1fcd1378ec1fff596ac380c96dd
kanekodaichi <knkdic.bfc@gmail.com>
2021-12-16 20:34:59 +0900

    turbolinksを無効化した

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,12 +4,12 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+//import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 require('../teams.js')
 
 Rails.start()
-Turbolinks.start()
+//Turbolinks.start()
 ActiveStorage.start()
