22a7f2647dc5a1f28497c5922ce26f435bd49531
kanekodaichi <knkdic.bfc@gmail.com>
2021-12-23 16:06:45 +0900

    試合結果一覧ページの見た目を調整

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -3,7 +3,13 @@
 class MatchesController < ApplicationController
 
   def index
-    @favorite_teams = current_user.favorites_teams
+    favorite_teams = current_user.favorites_teams
+    matches = []
+    favorite_teams.each do |favorite_team|
+      match = favorite_team.last_match(favorite_team.id)
+      matches << match
+    end
+    @matches = matches.uniq
   end
 
 end
