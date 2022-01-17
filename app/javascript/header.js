10db0d849420856bc0269307dc0ee47c8d3e1eb0
kanekodaichi <knkdic.bfc@gmail.com>
2022-01-17 22:33:48 +0900

    バーガーメニューが動くようにJSを追加

---
+++ app/javascript/header.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const $navbarBurgers = document.querySelectorAll(".navbar-burger");
+  
+    if ($navbarBurgers.length > 0) {
+      $navbarBurgers.forEach((el) => {
+        el.addEventListener("click", () => {
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
+  
+          el.classList.toggle("is-active");
+          $target.classList.toggle("is-active");
+        });
+      });
+    }
+  });
