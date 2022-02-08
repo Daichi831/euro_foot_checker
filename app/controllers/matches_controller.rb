b5b3ce34ee0adaa5a59bb743980909840d0b9f39
kanekodaichi <knkdic.bfc@gmail.com>
2022-02-08 13:52:39 +0900

    メッセージが表示されるように修正

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,16 +1,19 @@
 # frozen_string_literal: true
 
 class MatchesController < ApplicationController
-  before_action :authenticate_user!
   
   def index
-    favorite_teams = current_user.favorites_teams
-    matches = []
-    favorite_teams.each do |favorite_team|
-      match = favorite_team.last_match(favorite_team.id)
-      matches << match
+    if user_signed_in?
+      favorite_teams = current_user.favorites_teams
+      matches = []
+      favorite_teams.each do |favorite_team|
+        match = favorite_team.last_match(favorite_team.id)
+        matches << match
+      end
+      @matches = matches.uniq
+    else
+      redirect_to new_user_session_path
     end
-    @matches = matches.uniq
   end
 
 end
