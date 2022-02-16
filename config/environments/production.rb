dbd08b05c63ac41c969c22dfb8962275916041d8
kanekodaichi <knkdic.bfc@gmail.com>
2022-02-16 19:28:20 +0900

    credentials.yml.encを修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,8 +71,8 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'gmail.com',
-    user_name: Rails.application.credentials.mailer[:user_name],
-    password: Rails.application.credentials.mailer[:password],
+    user_name: Rails.application.credentials.gmail[:user_name],
+    password: Rails.application.credentials.gmail[:password],
     authentication: 'plain',
     enable_starttle_auto: true
   }
